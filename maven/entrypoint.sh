2bbdd8bdf0cc97131750c70280c6fab50b3129f1
EC2 Default User <ec2-user@ip-172-31-40-29.us-east-2.compute.internal>
2018-02-12 10:19:53 +0000

    java webapp changes

--- maven/entrypoint.sh
+++ maven/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sleep 30
-curl --upload-file target/my-app-1.0-SNAPSHOT.jar "http://admin1:admin1@sample-stack_tomcat:8080/manager/text/deploy?path=/sample&update=true"
+curl --upload-file target/webappRunnerSample.war "http://admin1:admin1@sample-stack_tomcat:8080/manager/text/deploy?path=/helloworld&update=true"
 apachectl -D FOREGROUND
 
